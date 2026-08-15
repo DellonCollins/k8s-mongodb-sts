8382e29734f1463e6ff57ceaf0ca4ae2644a5c0e
DellonCollins <collins.dellon@gmail.com>
2026-08-15 16:14:57 -0400

    corrected secret name for secret file

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 # create resources
 kubectl apply -f mongo-configmap.yaml,mongodb-pv.yaml,mongodb-sts.yaml,mongodb-service.yaml,mongo-express-d.yaml,mongo-express-service.yaml
-kubectl create secret generic mongodb-config --from-env-file=mongodb.env
+kubectl create secret generic mongodb-secret --from-env-file=mongodb.env
 
 # initiate db replica set
 kubectl exec -it mongodb-0 -- mongosh -u username -p password --eval "$(cat ./initiate_mongo_replica.txt)"
+
+# expose the mongo-express service
+minikube service mongo-express-service
