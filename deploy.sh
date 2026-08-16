85a9da49cd51e9b400242a51544f6bc03063091c
DellonCollins <collins.dellon@gmail.com>
2026-08-15 21:11:11 -0400

    fixed yq bug with at rest encryption deployment. Set output to yaml and ensured that kube-apiserver was the first value under command

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,14 @@
 # create resources
-kubectl apply -f mongo-configmap.yaml,mongodb-pv.yaml,mongodb-sts.yaml,mongodb-service.yaml,mongo-express-d.yaml,mongo-express-service.yaml
+kubectl apply -f mongo-configmap.yaml,mongodb-pv.yaml,mongodb-service.yaml,mongodb-sts.yaml,mongo-express-service.yaml,mongo-express-d.yaml
 kubectl create secret generic mongodb-secret --from-env-file=mongodb.env
 
 # initiate db replica set
-kubectl exec -it mongodb-0 -- mongosh -u username -p password --eval "$(cat ./initiate_mongo_replica.txt)"
+
 
 # expose the mongo-express service
 minikube service mongo-express-service
+
+# initiate db replica set
+repl_set_cmd='kubectl exec -it mongodb-0 -- mongosh -u username -p password --eval "$(cat ./initiate_mongo_replica.txt)"'
+echo Run this command to initiate the replica set once all pods are online
+echo $repl_set_cmd
